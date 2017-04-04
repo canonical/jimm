62f4664666a14381e62fd0895e9407ba8a9925dd
Francesco Banconi <francesco.banconi@canonical.com>
2017-04-04 13:07:30 +0200

    Implement the /gui-archive API endpoint.

--- internal/jujuapi/api.go
+++ internal/jujuapi/api.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/juju/httprequest"
+	jujuparams "github.com/juju/juju/apiserver/params"
 	"github.com/julienschmidt/httprouter"
 	"github.com/uber-go/zap"
 	"golang.org/x/net/context"
@@ -98,4 +99,14 @@ func (h *handler) GUI(p httprequest.Params, arg *guiRequest) error {
 	}
 	http.Redirect(p.Response, p.Request, fmt.Sprintf("%s/u/%s/%s", h.params.GUILocation, m.Path.User, m.Path.Name), http.StatusMovedPermanently)
 	return nil
+}
+
+type guiArchiveRequest struct {
+	httprequest.Route `httprequest:"GET /gui-archive"`
+}
+
+// GUIArchive provides information on GUI versions for compatibility with Juju
+// controllers. In this case, no versions are returned.
+func (h *handler) GUIArchive(*guiArchiveRequest) (jujuparams.GUIArchiveResponse, error) {
+	return jujuparams.GUIArchiveResponse{}, nil
 }
