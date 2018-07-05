dd1f780f1b97073f5f6c359e9510c5b413778006
Martin Hilton <martin.hilton@canonical.com>
2018-07-05 11:54:09 +0100

    internal/admin: add acl manager

---
+++ internal/admin/api.go
@@ -0,0 +1,70 @@
+// Copyright 2018 Canonical Ltd.
+
+package admin
+
+import (
+	"net/http"
+
+	"github.com/juju/aclstore"
+	"github.com/juju/httprequest"
+	"github.com/juju/simplekv/mgosimplekv"
+	"github.com/julienschmidt/httprouter"
+	"golang.org/x/net/context"
+	errgo "gopkg.in/errgo.v1"
+
+	"github.com/CanonicalLtd/jem/internal/auth"
+	"github.com/CanonicalLtd/jem/internal/jemerror"
+	"github.com/CanonicalLtd/jem/internal/jemserver"
+	"github.com/CanonicalLtd/jem/params"
+)
+
+// NewAPIHandler creates a new admin API handler.
+func NewAPIHandler(ctx context.Context, params jemserver.HandlerParams) ([]httprequest.Handler, error) {
+	m, err := aclstore.NewManager(ctx, aclstore.Params{
+		Store:    params.ACLStore,
+		RootPath: "/admin/acls",
+		Authenticate: func(ctx context.Context, w http.ResponseWriter, req *http.Request) (aclstore.Identity, error) {
+			authenticator := params.AuthenticatorPool.Authenticator(ctx)
+			defer authenticator.Close()
+			ctx, err := authenticator.AuthenticateRequest(ctx, req)
+			if err != nil {
+				status, body := jemerror.Mapper(err)
+				httprequest.WriteJSON(w, status, body)
+				return nil, errgo.Mask(err, errgo.Any)
+			}
+			return identity{ctx}, nil
+		},
+		InitialAdminUsers: []string{string(params.ControllerAdmin)},
+	})
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	f := func(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+		ctx, close := params.SessionPool.ContextWithSession(req.Context())
+		defer close()
+		s := params.SessionPool.Session(ctx)
+		defer s.Close()
+		ctx = mgosimplekv.ContextWithSession(ctx, s)
+		req = req.WithContext(ctx)
+		m.ServeHTTP(w, req)
+	}
+	return []httprequest.Handler{
+		{"GET", "/admin/acls/*path", f},
+		{"POST", "/admin/acls/*path", f},
+		{"PUT", "/admin/acls/*path", f},
+	}, nil
+}
+
+type identity struct {
+	ctx context.Context
+}
+
+func (i identity) Allow(_ context.Context, acl []string) (bool, error) {
+	if err := auth.CheckACL(i.ctx, acl); err != nil {
+		if errgo.Cause(err) == params.ErrUnauthorized {
+			return false, nil
+		}
+		return false, errgo.Mask(err)
+	}
+	return true, nil
+}
---
+++ internal/admin/api_test.go
@@ -0,0 +1,124 @@
+// Copyright 2018 Canonical Ltd.
+
+package admin_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/juju/aclstore/aclclient"
+	"github.com/juju/aclstore/params"
+	"github.com/juju/httprequest"
+	jc "github.com/juju/testing/checkers"
+	"golang.org/x/net/context"
+	gc "gopkg.in/check.v1"
+	errgo "gopkg.in/errgo.v1"
+
+	"github.com/CanonicalLtd/jem/internal/apitest"
+)
+
+type APISuite struct {
+	apitest.Suite
+
+	baseURL string
+	srv     *httptest.Server
+}
+
+var _ = gc.Suite(&APISuite{})
+
+func (s *APISuite) SetUpTest(c *gc.C) {
+	s.Suite.SetUpTest(c)
+	s.srv = httptest.NewServer(s.JEMSrv)
+	s.baseURL = s.srv.URL + "/admin/acls"
+}
+
+func (s *APISuite) TearDownTest(c *gc.C) {
+	if s.srv != nil {
+		s.srv.Close()
+	}
+	s.Suite.TearDownTest(c)
+}
+
+func (s *APISuite) TestGetACL(c *gc.C) {
+	client := aclclient.New(aclclient.NewParams{
+		BaseURL: s.baseURL,
+		Doer:    doadaptor{s.IDMSrv.Client("controller-admin")},
+	})
+	acls, err := client.GetACL(context.Background(), &params.GetACLRequest{
+		Name: "admin",
+	})
+	c.Assert(err, gc.Equals, nil)
+	c.Assert(acls, jc.DeepEquals, &params.GetACLResponse{Users: []string{"controller-admin"}})
+}
+
+func (s *APISuite) TestUnauthorized(c *gc.C) {
+	client := aclclient.New(aclclient.NewParams{
+		BaseURL: s.baseURL,
+		Doer:    doadaptor{s.IDMSrv.Client("bob")},
+	})
+	acls, err := client.GetACL(context.Background(), &params.GetACLRequest{
+		Name: "admin",
+	})
+	c.Assert(err, gc.ErrorMatches, `Get http.*/admin/acls/admin: forbidden`)
+	c.Assert(acls, gc.IsNil)
+}
+
+func (s *APISuite) TestSetACL(c *gc.C) {
+	client := aclclient.New(aclclient.NewParams{
+		BaseURL: s.baseURL,
+		Doer:    doadaptor{s.IDMSrv.Client("controller-admin")},
+	})
+	err := client.SetACL(context.Background(), &params.SetACLRequest{
+		Name: "admin",
+		Body: params.SetACLRequestBody{
+			Users: []string{"controller-admin", "bob"},
+		},
+	})
+	c.Assert(err, gc.Equals, nil)
+	acls, err := client.GetACL(context.Background(), &params.GetACLRequest{
+		Name: "admin",
+	})
+	c.Assert(err, gc.Equals, nil)
+	c.Assert(acls, jc.DeepEquals, &params.GetACLResponse{Users: []string{"bob", "controller-admin"}})
+}
+
+func (s *APISuite) TestModifyACL(c *gc.C) {
+	client := aclclient.New(aclclient.NewParams{
+		BaseURL: s.baseURL,
+		Doer:    doadaptor{s.IDMSrv.Client("controller-admin")},
+	})
+	err := client.ModifyACL(context.Background(), &params.ModifyACLRequest{
+		Name: "admin",
+		Body: params.ModifyACLRequestBody{
+			Add: []string{"alice"},
+		},
+	})
+	c.Assert(err, gc.Equals, nil)
+	acls, err := client.GetACL(context.Background(), &params.GetACLRequest{
+		Name: "admin",
+	})
+	c.Assert(err, gc.Equals, nil)
+	c.Assert(acls, jc.DeepEquals, &params.GetACLResponse{Users: []string{"alice", "controller-admin"}})
+}
+
+type doer interface {
+	httprequest.Doer
+	httprequest.DoerWithBody
+}
+
+type doadaptor struct {
+	doer
+}
+
+func (d doadaptor) Do(req *http.Request) (*http.Response, error) {
+	if req.Body == nil {
+		return d.doer.Do(req)
+	}
+	body, ok := req.Body.(io.ReadSeeker)
+	if !ok {
+		return nil, errgo.Newf("invalid body")
+	}
+	req.Body = nil
+	return d.doer.DoWithBody(req, body)
+}
