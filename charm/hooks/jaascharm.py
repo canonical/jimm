fdb3fec22015b58644337f55b93d8a6580a1d078
Ales Stimec <ales.stimec@canonical.com>
2020-04-14 12:54:19 +0200

    Added Juju Dashboard resource to the charm.

--- charm/hooks/jaascharm.py
+++ charm/hooks/jaascharm.py
@@ -66,10 +66,14 @@ def install(binary=None):
 
     new_resource_path = resource_path + '.new'
     old_resource_path = resource_path + '.old'
+    new_dashboard_path = dashboard_path() + '.new'
+    old_dashboard_path = dashboard_path() + '.old'
     # Remove possible remnants of a failed install and
     # the previous previous resource directory.
     shutil.rmtree(new_resource_path, ignore_errors=True)
     shutil.rmtree(old_resource_path, ignore_errors=True)
+    shutil.rmtree(new_dashboard_path, ignore_errors=True)
+    shutil.rmtree(old_dashboard_path, ignore_errors=True)
 
     hookenv.status_set('maintenance', 'getting service resource')
     resource_file = hookenv.resource_get('service')
@@ -104,6 +108,19 @@ def install(binary=None):
         'resource_path': resource_path,
         'service': service,
     }
+
+    dashboard_file = hookenv.resource_get('dashboard')
+    if dashboard_file:
+        hookenv.status_set('maintenance', 'installing dashboard')
+        with tarfile.open(dashboard_file) as tf:
+            tf.extractall(new_dashboard_path)
+            # Change the owner/group of all extracted files to root/wheel.
+            for name in tf.getnames():
+                os.chown(os.path.join(new_dashboard_path, name), 0, 0)
+        if os.path.exists(dashboard_path()):
+            os.rename(dashboard_path(), old_dashboard_path)
+        os.rename(new_dashboard_path, dashboard_path())
+
     templating.render(
         'service',
         service_path,
@@ -128,6 +145,22 @@ def update_config(config):
        None then that value will be removed from the configuration file.
        It reports whether the config file was changed.
     """
+
+    """Update the config.js file for the Juju Dashboard.
+    """
+    config_js_path = os.path.join(dashboard_path(), 'config.js')
+    config = hookenv.config()
+    dashboard_context = {
+        'base_controller_url': config['controller-url'],
+        'base_app_url': '/dashboard/',
+        'identity_provider_available': str(len(config['identity-location']) != 0).lower(),
+    }
+    templating.render(
+        'dashboard-config',
+        config_js_path,
+        dashboard_context
+    )
+
     path = _config_path()
     data = {}
     changed = True
@@ -156,6 +189,7 @@ def update_config(config):
         group=_service(),
         perms=0o640,
     )
+
     return True
 
 
@@ -272,6 +306,11 @@ def _root():
 def _config_path():
     """Return the path to the service configuration file."""
     return os.path.join(_root(), 'config.yaml')
+
+
+def dashboard_path():
+    """Return the path to Juju Dashboard folder."""
+    return os.path.join(_root(), 'dashboard')
 
 
 def _service_failed():
