7e44902beab560872a0ecdd0564c880574a72141
Martin Hilton <martin.hilton@canonical.com>
2020-11-27 11:26:42 +0000

    internal/db: rework the Database object
    Change the Database object such that the zero value is, somewhat, useful
    removing the need for a constructor. Also add a Transaction method to
    allow Database operations to be run in transactions.

--- internal/db/db.go
+++ internal/db/db.go
@@ -24,16 +24,26 @@ type Database struct {
 	// to the current database version. The value of migrated should always
 	// be read using atomic.LoadUint32 and will contain a 0 if the
 	// migration is yet to be run, or 1 if it has been run successfully.
-	migrated *uint32
+	migrated uint32
 }
 
-// NewDatabase creates a new Database using the given gorm.DB to store the
-// data.
-func NewDatabase(db *gorm.DB) Database {
-	return Database{
-		DB:       db,
-		migrated: new(uint32),
+// Transaction starts a new transaction using the database. This allows
+// a set of changes to be performed as a single atomic unit. All of the
+// transaction steps should be performed in the given function, if this
+// function returns an error then all changes in the transaction will be
+// aborted and the error returned. Transactions may be nested.
+//
+// Attempting to start a transaction on an unmigrated database will result
+// in an error with a code of errors.CodeUpgradeInProgress.
+func (d *Database) Transaction(f func(*Database) error) error {
+	if err := d.ready(); err != nil {
+		return err
 	}
+	return d.DB.Transaction(func(tx *gorm.DB) error {
+		d := *d
+		d.DB = tx
+		return f(&d)
+	})
 }
 
 // Migrate migrates the configured database to have the structure required
@@ -45,23 +55,23 @@ func NewDatabase(db *gorm.DB) Database {
 // then the migration will be performed no matter what the current version
 // is. The force parameter should only be set when the migration is
 // initiated by a user request.
-func (d Database) Migrate(ctx context.Context, force bool) error {
+func (d *Database) Migrate(ctx context.Context, force bool) error {
 	const op = errors.Op("db.Migrate")
-	if d.DB == nil {
+	if d == nil || d.DB == nil {
 		return errors.E(op, errors.CodeServerConfiguration, "database not configured")
 	}
 	db := d.DB.WithContext(ctx)
 	if err := db.AutoMigrate(&dbmodel.Version{}); err != nil {
-		return errors.E(op, errorCode(err), err)
+		return errors.E(op, dbError(err))
 	}
 	v := dbmodel.Version{Component: dbmodel.Component}
 	if err := db.FirstOrCreate(&v).Error; err != nil {
-		return errors.E(op, errorCode(err), err)
+		return errors.E(op, dbError(err))
 	}
 	if dbmodel.Major == v.Major && dbmodel.Minor <= v.Minor {
 		// The database is already at, or past, our current version.
 		// Nothing to do.
-		atomic.StoreUint32(d.migrated, 1)
+		atomic.StoreUint32(&d.migrated, 1)
 		return nil
 	}
 	if v.Major != dbmodel.Major && !force && v.Major != 0 {
@@ -86,26 +96,26 @@ func (d Database) Migrate(ctx context.Context, force bool) error {
 		&dbmodel.UserModelAccess{},
 	)
 	if err != nil {
-		return errors.E(op, errorCode(err), err)
+		return errors.E(op, dbError(err))
 	}
 
 	v.Major = dbmodel.Major
 	v.Minor = dbmodel.Minor
 	if err := db.Save(&v).Error; err != nil {
-		return errors.E(op, errorCode(err), err)
+		return errors.E(op, dbError(err))
 	}
-	atomic.StoreUint32(d.migrated, 1)
+	atomic.StoreUint32(&d.migrated, 1)
 	return nil
 }
 
 // ready checks that the database is ready to accept requests. An error is
 // returned if the database is not yet initialised.
-func (d Database) ready(op errors.Op) error {
-	if d.DB == nil {
-		return errors.E(op, errors.CodeServerConfiguration, "database not configured")
+func (d *Database) ready() error {
+	if d == nil || d.DB == nil {
+		return errors.E(errors.CodeServerConfiguration, "database not configured")
 	}
-	if atomic.LoadUint32(d.migrated) == 0 {
-		return errors.E(op, errors.CodeUpgradeInProgress)
+	if atomic.LoadUint32(&d.migrated) == 0 {
+		return errors.E(errors.CodeUpgradeInProgress)
 	}
 	return nil
 }
--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -9,18 +9,17 @@ import (
 	qt "github.com/frankban/quicktest"
 
 	"github.com/CanonicalLtd/jimm/internal/db"
+	"github.com/CanonicalLtd/jimm/internal/dbmodel"
 	"github.com/CanonicalLtd/jimm/internal/errors"
 )
 
 // dbSuite contains a suite of database tests that are run against
 // different database engines.
 type dbSuite struct {
-	Database db.Database
+	Database *db.Database
 }
 
 func (s *dbSuite) TestMigrate(c *qt.C) {
-	c.Parallel()
-
 	// Migrate from an empty database should work.
 	err := s.Database.Migrate(context.Background(), false)
 	c.Assert(err, qt.IsNil)
@@ -38,4 +37,37 @@ func TestMigrateUnconfiguredDatabase(t *testing.T) {
 	err := database.Migrate(context.Background(), false)
 	c.Check(err, qt.ErrorMatches, `database not configured`)
 	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeServerConfiguration)
+}
+
+func TestTransactionUnconfiguredDatabase(t *testing.T) {
+	c := qt.New(t)
+
+	var database db.Database
+	err := database.Transaction(func(d *db.Database) error {
+		return errors.E("unexpected function call")
+	})
+	c.Check(err, qt.ErrorMatches, `database not configured`)
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeServerConfiguration)
+}
+
+func (s *dbSuite) TestTransaction(c *qt.C) {
+	err := s.Database.Transaction(func(d *db.Database) error {
+		return errors.E("unexpected function call")
+	})
+	c.Check(err, qt.ErrorMatches, `upgrade in progress`)
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeUpgradeInProgress)
+
+	err = s.Database.Migrate(context.Background(), false)
+	c.Assert(err, qt.IsNil)
+
+	err = s.Database.Transaction(func(d *db.Database) error {
+		c.Check(d, qt.Not(qt.Equals), s.Database)
+		return d.GetUser(context.Background(), &dbmodel.User{Username: "bob@external"})
+	})
+	c.Assert(err, qt.IsNil)
+
+	err = s.Database.Transaction(func(d *db.Database) error {
+		return errors.E("test error")
+	})
+	c.Check(err, qt.ErrorMatches, `test error`)
 }
