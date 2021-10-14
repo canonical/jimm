28c7063f321ade9386d36662c71d4db256526e3b
Martin Hilton <martin.hilton@canonical.com>
2021-10-14 12:27:27 +0100

    improve error message

--- internal/jimm/model_test.go
+++ internal/jimm/model_test.go
@@ -829,7 +829,7 @@ machines:
 		CloudRegion:        "test-region-1",
 		CloudCredentialTag: names.NewCloudCredentialTag("test-cloud/alice@external/test-credential-1").String(),
 	},
-	expectError: "failed to update cloud credential",
+	expectError: "failed to update cloud credential: a silly error",
 }}
 
 func TestAddModel(t *testing.T) {
