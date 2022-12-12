06bf951abc559ef3cb0fa20aee70817be0fe98d9
ale8k <alexanderkilroy@outlook.com>
2022-12-12 12:38:29 +0000

    Add AppRole for JIMM

--- local/vault/init.sh
+++ local/vault/init.sh
@@ -7,15 +7,22 @@
 vault server -dev -config=/vault/config/vault.hcl &
 sleep 2
 
-# echo "Enabling AppRole auth"
-# # Enable auth
-# vault auth enable approle
-# # Create role
-# vault write auth/approle/role/jimm-app \
-#     secret_id_ttl=10m \
-#     token_num_uses=10 \
-#     token_ttl=20m \
-#     token_max_ttl=30m \
-#     secret_id_num_uses=40
+# Login.
+echo "token" | vault login -
+
+# Set address for local client.
+export VAULT_ADDR="http://localhost:8200"
+
+echo "Enabling AppRole auth"
+# Enable auth
+vault auth enable approle
+echo "Creating jimm-app AppRole"
+# Create role
+vault write auth/approle/role/jimm-app \
+    secret_id_ttl=10m \
+    token_num_uses=10 \
+    token_ttl=20m \
+    token_max_ttl=30m \
+    secret_id_num_uses=40
 
 wait
