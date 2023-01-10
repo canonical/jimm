c7567c6c6ae49289da205f5dec6e251d28ad025b
Kian Parvin <kian.parvin@canonical.com>
2023-01-10 15:18:25 +0200

    Moved tests

--- internal/jujuapi/access_control_test.go
+++ internal/jujuapi/access_control_test.go
@@ -1,9 +1,12 @@
 package jujuapi_test
 
 import (
+	"fmt"
+
 	"github.com/CanonicalLtd/jimm/api"
 	apiparams "github.com/CanonicalLtd/jimm/api/params"
 	jc "github.com/juju/testing/checkers"
+	"github.com/stretchr/testify/assert"
 	gc "gopkg.in/check.v1"
 )
 
@@ -45,4 +48,48 @@ func (s *accessControlSuite) TestRenameGroup(c *gc.C) {
 		NewName: "renamed-group",
 	})
 	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (s *accessControlSuite) TestRemoveGroup(c *gc.C) {
+	conn := s.open(c, nil, "alice")
+	defer conn.Close()
+
+	client := api.NewClient(conn)
+
+	err := client.RemoveGroup(&apiparams.RemoveGroupRequest{
+		Name: "test-group",
+	})
+	c.Assert(err, gc.ErrorMatches, ".*not found.*")
+
+	err = client.AddGroup(&apiparams.AddGroupRequest{Name: "test-group"})
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = client.RemoveGroup(&apiparams.RemoveGroupRequest{
+		Name: "test-group",
+	})
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (s *accessControlSuite) TestListGroups(c *gc.C) {
+	conn := s.open(c, nil, "alice")
+	defer conn.Close()
+
+	client := api.NewClient(conn)
+
+	for i := 0; i < 3; i++ {
+		err := client.AddGroup(&apiparams.AddGroupRequest{Name: fmt.Sprint("test-group", i)})
+		c.Assert(err, jc.ErrorIsNil)
+	}
+	err := client.AddGroup(&apiparams.AddGroupRequest{Name: "aaaFinalGroup"})
+	c.Assert(err, jc.ErrorIsNil)
+
+	groups, err := client.ListGroups()
+	c.Assert(err, jc.ErrorIsNil)
+	assert.ElementsMatch(c)
+	c.Assert(len(groups), gc.Equals, 4)
+	// groups should be returned in ascending order of name
+	c.Assert(groups[0].Name, gc.Equals, "aaaFinalGroup")
+	c.Assert(groups[1].Name, gc.Equals, "test-group0")
+	c.Assert(groups[2].Name, gc.Equals, "test-group1")
+	c.Assert(groups[3].Name, gc.Equals, "test-group2")
 }
