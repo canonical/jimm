f411fbab62c577a3e6c2cb5cd9a472a667fe7191
Kian Parvin <kian.parvin@canonical.com>
2023-01-12 15:22:11 +0200

    Added test for removing tuples

--- internal/openfga/openfga_test.go
+++ internal/openfga/openfga_test.go
@@ -75,6 +75,44 @@ func (suite *openFGATestSuite) TestWritingTuplesToOFGADetectsSucceeds() {
 	assert.Equal(t, user2, lastInsertedTuple.GetUser())
 }
 
+func (suite *openFGATestSuite) TestDeletingTuplesToOFGADetectsSucceeds() {
+	t := suite.T()
+	ctx := context.Background()
+
+	//Create tuples before writing to db
+	uuid1, _ := uuid.NewRandom()
+	user1 := fmt.Sprintf("user:%s", uuid1)
+	key1 := suite.ofgaClient.CreateTupleKey(user1, "member", "group:pokemon")
+	uuid2, _ := uuid.NewRandom()
+	user2 := fmt.Sprintf("user:%s", uuid2)
+	key2 := suite.ofgaClient.CreateTupleKey(user2, "member", "group:pokemon")
+
+	//Delete before insert should fail
+	err := suite.ofgaClient.DeleteRelations(ctx, key1, key2)
+	_, ok := err.(openfga.FgaApiValidationError)
+	assert.True(t, ok)
+	assert.Contains(t, err.Error(), "cannot delete a tuple which does not exist")
+
+	err = suite.ofgaClient.AddRelations(ctx, key1, key2)
+	assert.NoError(t, err)
+
+	//Delete after insert should succeed.
+	err = suite.ofgaClient.DeleteRelations(ctx, key1, key2)
+	assert.NoError(t, err)
+	changes, _, err := suite.ofgaApi.ReadChanges(ctx).Type_("group").Execute()
+	assert.NoError(t, err)
+
+	secondToLastInsertedTuple := changes.GetChanges()[len(changes.GetChanges())-2]
+	secondLastKey := secondToLastInsertedTuple.GetTupleKey()
+	assert.Equal(t, user1, secondLastKey.GetUser())
+	assert.Equal(t, openfga.DELETE, secondToLastInsertedTuple.GetOperation())
+
+	lastInsertedTuple := changes.GetChanges()[len(changes.GetChanges())-1]
+	lastKey := lastInsertedTuple.GetTupleKey()
+	assert.Equal(t, user2, lastKey.GetUser())
+	assert.Equal(t, openfga.DELETE, lastInsertedTuple.GetOperation())
+}
+
 func TestOpenFGATestSuite(t *testing.T) {
 	suite.Run(t, new(openFGATestSuite))
 }
