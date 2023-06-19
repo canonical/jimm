39e4bbba68b264f4c414215468f07ebf2c2feff7
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2023-06-19 11:45:05 +0100

    CSS-3895 Add upgrade test to `jimm-k8s` charm (#957)
    * Update log entry
    
    Signed-off-by: babakks <babak.k.shandiz@canonical.com>
    
    * Add upgrade test
    
    Signed-off-by: babakks <babak.k.shandiz@canonical.com>
    
    * Apply linter suggestion
    
    Signed-off-by: babakks <babak.k.shandiz@canonical.com>
    
    * Add `local_charm` support to upgrade test
    
    Signed-off-by: babakks <babak.k.shandiz@canonical.com>
    
    ---------
    
    Signed-off-by: babakks <babak.k.shandiz@canonical.com>

--- charms/jimm-k8s/tests/integration/test_upgrade.py
+++ charms/jimm-k8s/tests/integration/test_upgrade.py
@@ -20,23 +20,17 @@ APP_NAME = "juju-jimm-k8s"
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest, local_charm):
-    """Build the charm-under-test and deploy it together with related charms.
+async def test_upgrade_running_application(ops_test: OpsTest, local_charm):
+    """Deploy latest published charm and upgrade it with charm-under-test.
 
-    Assert on the unit status before any relations/configurations take place.
+    Assert on the application status and health check endpoint after upgrade/refresh took place.
     """
-    # (Optionally build) and deploy charm from local source folder
-    if local_charm:
-        charm = Path(utils.get_local_charm()).resolve()
-    else:
-        charm = await ops_test.build_charm(".")
-    resources = {"jimm-image": "localhost:32000/jimm:latest"}
 
     # Deploy the charm and wait for active/idle status
     logger.debug("deploying charms")
     await ops_test.model.deploy(
-        charm,
-        resources=resources,
+        METADATA["name"],
+        channel="edge",
         application_name=APP_NAME,
         series="focal",
         config={
@@ -71,10 +65,10 @@ async def test_build_and_deploy(ops_test: OpsTest, local_charm):
     )
 
     logger.info("adding traefik relation")
-    await ops_test.model.integrate("{}:ingress".format(APP_NAME), "traefik")
+    await ops_test.model.relate("{}:ingress".format(APP_NAME), "traefik")
 
     logger.info("adding openfga postgresql relation")
-    await ops_test.model.integrate("openfga:database", "postgresql:database")
+    await ops_test.model.relate("openfga:database", "postgresql:database")
 
     logger.info("waiting for openfga")
     await ops_test.model.wait_for_idle(
@@ -93,10 +87,10 @@ async def test_build_and_deploy(ops_test: OpsTest, local_charm):
         time.sleep(2)
 
     logger.info("adding openfga relation")
-    await ops_test.model.integrate(APP_NAME, "openfga")
+    await ops_test.model.relate(APP_NAME, "openfga")
 
     logger.info("adding postgresql relation")
-    await ops_test.model.integrate(APP_NAME, "postgresql:database")
+    await ops_test.model.relate(APP_NAME, "postgresql:database")
 
     logger.info("waiting for jimm")
     await ops_test.model.wait_for_idle(
@@ -122,3 +116,42 @@ async def test_build_and_deploy(ops_test: OpsTest, local_charm):
             time.sleep(2)
 
     assert ops_test.model.applications[APP_NAME].status == "active"
+
+    # Starting upgrade/refresh
+    logger.info("starting upgrade test")
+
+    # Build and deploy charm from local source folder
+    logger.info("building local charm")
+
+    # (Optionally build) and deploy charm from local source folder
+    if local_charm:
+        charm = Path(utils.get_local_charm()).resolve()
+    else:
+        charm = await ops_test.build_charm(".")
+    resources = {"jimm-image": "localhost:32000/jimm:latest"}
+
+    # Deploy the charm and wait for active/idle status
+    logger.info("refreshing running application with the new local charm")
+
+    await ops_test.model.applications[APP_NAME].refresh(
+        path=charm,
+        resources=resources,
+    )
+
+    logger.info("waiting for the upgraded unit to be ready")
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="active",
+            timeout=60,
+        )
+
+    assert ops_test.model.applications[APP_NAME].status == "active"
+
+    logger.info("checking status of the running unit")
+    upgraded_jimm_unit = await utils.get_unit_by_name(APP_NAME, "0", ops_test.model.units)
+
+    health = await upgraded_jimm_unit.run("curl -i http://localhost:8080/debug/status")
+    await health.wait()
+    assert health.results.get("return-code") == 0
+    assert health.results.get("stdout").strip().splitlines()[0].endswith("200 OK")
