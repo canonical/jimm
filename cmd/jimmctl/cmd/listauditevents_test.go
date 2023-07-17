e08c31663e697f3d2212fbf8a6bb774cdbe66e3b
Kian Parvin <kian.parvin@canonical.com>
2023-07-17 17:37:32 +0200

    Revert "Test change"
    This reverts commit b8282f21ad5d3b3524260157e9367bfb023aa3f5.

--- cmd/jimmctl/cmd/listauditevents_test.go
+++ cmd/jimmctl/cmd/listauditevents_test.go
@@ -54,7 +54,7 @@ func (s *listAuditEventsSuite) TestListAuditEventsSuperuser(c *gc.C) {
     cloud: cloud-`+jimmtest.TestCloudName+`
     cloud-credential: cloudcred-`+jimmtest.TestCloudName+`_charlie@external_cred
     name: model-2
-    owner: .*
+    owner: user-charlie@external
     region: `+jimmtest.TestCloudRegionName+`
 `)
 }
