00de18307bfe1faefd416a6e32c015292014bb57
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2023-07-27 11:55:45 +0100

    Remove lxc proxy setup
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- local/jimm/setup-controller.sh
+++ local/jimm/setup-controller.sh
@@ -25,5 +25,3 @@ printf "$CLOUDINIT_TEMPLATE" "$(lxc network get lxdbr0 ipv4.address | cut -f1 -d
 echo "Bootstrapping controller"
 juju bootstrap localhost "${CONTROLLER_NAME}" --config allow-model-access=true --config "${CLOUDINIT_FILE}" --config login-token-refresh-url=https://jimm.localhost/.well-known/jwks.json
 CONTROLLER_ID=$(juju show-controller --format json | jq --arg name "${CONTROLLER_NAME}" '.[$name]."controller-machines"."0"."instance-id"' | tr -d '"')
-echo "Adding proxy to LXC instance ${CONTROLLER_ID}"
-lxc config device add "${CONTROLLER_ID}" myproxy proxy listen=tcp:0.0.0.0:443 connect=tcp:127.0.0.1:443 bind=instance
