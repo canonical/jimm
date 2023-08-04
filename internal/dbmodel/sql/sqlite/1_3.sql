4bbd30bfaff6d845eedc04ee9ac49e4bc2037de3
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2023-08-04 11:30:58 +0100

    Remove unnecessary transaction block
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- internal/dbmodel/sql/sqlite/1_3.sql
+++ internal/dbmodel/sql/sqlite/1_3.sql
@@ -3,12 +3,10 @@
 --   - https://www.sqlite.org/lang_altertable.html
 --   - http://web.archive.org/web/20230718062623/https://www.sqlite.org/lang_altertable.html
 
-begin transaction;
 PRAGMA schema_version;
 PRAGMA writable_schema=ON;
 UPDATE sqlite_schema SET sql=REPLACE(sql,'cloud_name TEXT NOT NULL REFERENCES clouds (name)','cloud_name TEXT NOT NULL REFERENCES clouds (name) ON DELETE CASCADE') WHERE type='table' AND name='cloud_credentials';
 PRAGMA writable_schema=OFF;
 PRAGMA integrity_check;
-commit transaction;
 
 UPDATE versions SET major=1, minor=3 WHERE component='jimmdb';
