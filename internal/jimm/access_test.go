580b779ea0ce1b17ee7108bd7034fb4001d14572
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2023-08-21 11:28:42 +0100

    Update TODO with Jira card reference
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- internal/jimm/access_test.go
+++ internal/jimm/access_test.go
@@ -17,36 +17,15 @@ import (
 	"github.com/google/uuid"
 )
 
-// TODO(Kian): We could use a test as below to unit test the
-// auth function returned by JwtGenerator and ensure it correctly
-// generates a JWT. The JWTGenerator requires the JIMM server to have
-// a JWT service and cache setup, so we could either turn this into
-// an interface and mock it or have the test start a full JIMM server.
-// Also required an interface for authentication, mocked or Candid.
-// This is already tested in an integration test in jujuapi/websocket_test.go
-// func TestJwtGenerator(t *testing.T) {
-// 	c := qt.New(t)
-
-// 	client, _, _, err := jimmtest.SetupTestOFGAClient(c.Name())
-// 	c.Assert(err, qt.IsNil)
-
-// 	j := &jimm.JIMM{
-// 		UUID: uuid.NewString(),
-// 		Database: db.Database{
-// 			DB: jimmtest.MemoryDB(c, nil),
-// 		},
-// 		OpenFGAClient: client,
-// 		JWTService:    jimmjwx.NewJWTService(,),
-// 	}
-// 	ctx := context.Background()
-// 	m := &dbmodel.Model{}
-// 	authFunc := j.JwtGenerator(ctx, m)
-// 	loginReq := new(jujuparams.LoginRequest)
-// 	desiredPerms := map[string]interface{}{"model-123": "writer", "applicationoffer-123": "consumer"}
-// 	token, err := authFunc(loginReq, desiredPerms)
-// 	c.Assert(err, qt.IsNil)
-// 	//Check token is valid and has correct assertions.
-// }
+/*
+TODO: (CSS-5247) We could use a test as below to unit test the
+auth function returned by JwtGenerator and ensure it correctly
+generates a JWT. The JWTGenerator requires the JIMM server to have
+a JWT service and cache setup, so we could either turn this into
+an interface and mock it or have the test start a full JIMM server.
+Also required an interface for authentication, mocked or Candid.
+This is already tested in an integration test in jujuapi/websocket_test.go
+*/
 
 func TestAuditLogAccess(t *testing.T) {
 	c := qt.New(t)
