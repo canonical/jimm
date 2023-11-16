ceb4a4174205db2b28d73c22b11d1eba678297bd
Kian Parvin <kian.parvin@canonical.com>
2023-11-16 12:41:22 +0200

    Remove unused tables and columns

---
+++ internal/dbmodel/sql/postgres/1_4.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS user_application_offer_access;
+DROP TABLE IF EXISTS user_cloud_access;
+DROP TABLE IF EXISTS user_model_access;
+ALTER TABLE users DROP COLUMN controller_access;
+ALTER TABLE users DROP COLUMN audit_log_access;
+
+UPDATE versions SET major=1, minor=4 WHERE component='jimmdb';
