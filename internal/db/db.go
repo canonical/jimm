6d6ad87bde690b90e48c99b3168261770ab13e16
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2023-11-30 17:01:13 +0000

    Fix error message
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- internal/db/db.go
+++ internal/db/db.go
@@ -114,6 +114,18 @@ func (d *Database) ready() error {
 	return nil
 }
 
+// Close closes open connections to the underlying database backend.
+func (d *Database) Close() error {
+	sqlDB, err := d.DB.DB()
+	if err != nil {
+		return errors.E(err, "failed to get the internal DB object")
+	}
+	if err := sqlDB.Close(); err != nil {
+		return errors.E(err, "failed to close database connection")
+	}
+	return nil
+}
+
 // Now returns the current time as a valid sql.NullTime. The time that is
 // returned is in UTC and is truncated to milliseconds which is the
 // resolution supported on all databases.
@@ -122,16 +134,4 @@ func Now() sql.NullTime {
 		Time:  time.Now().UTC().Truncate(time.Millisecond),
 		Valid: true,
 	}
-}
-
-// Close closes open connections to the underlying database backend.
-func (d *Database) Close() error {
-	sqlDB, err := d.DB.DB()
-	if err != nil {
-		return errors.E(err, "failed to get the internal DB object")
-	}
-	if err := sqlDB.Close(); err != nil {
-		return errors.E(err, "failed to close template database connection")
-	}
-	return nil
 }
