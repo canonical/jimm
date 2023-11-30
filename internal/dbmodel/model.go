987069a788e60edab16c446282d0dc143bc6cddc
Kian Parvin <kian.parvin@canonical.com>
2023-11-30 16:36:47 +0200

    Added life enum

--- internal/dbmodel/model.go
+++ internal/dbmodel/model.go
@@ -15,6 +15,48 @@ import (
 	"github.com/canonical/jimm/internal/errors"
 )
 
+// Life values specify model life status
+type Life int
+
+// Enumerate all possible migration phases.
+const (
+	UNKNOWN Life = iota
+	ALIVE
+	DEAD
+	DYING
+	MIGRATING_INTERNAL
+	MIGRATING_AWAY
+)
+
+var lifeNames = []string{
+	"unknown",
+	"alive",
+	"dead",
+	"dying",
+	"migrating-internal",
+	"migrating-way",
+}
+
+// String returns the name of an model life constant.
+func (p Life) String() string {
+	i := int(p)
+	if i >= 0 && i < len(lifeNames) {
+		return lifeNames[i]
+	}
+	return "UNKNOWN"
+}
+
+// Parselife converts a string model life name
+// to its constant value.
+func ParseLife(target string) (Life, bool) {
+	for p, name := range lifeNames {
+		if target == name {
+			return Life(p), true
+		}
+	}
+	return UNKNOWN, false
+}
+
 // A Model is a juju model.
 type Model struct {
 	// Note this cannot use the standard gorm.Model as the soft-delete does
