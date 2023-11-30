7c3ab1e8b9a363aeec1c2804a5edb0a7c6499ea5
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2023-11-30 10:47:36 +0000

    Delete SQLite migration files
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- internal/dbmodel/sql/sqlite/1_4.sql
+++
@@ -1,9 +0,0 @@
--- 1_4.sql is a migration that removes unused tables and columns previously used for access checks.
-
-DROP TABLE IF EXISTS user_application_offer_access;
-DROP TABLE IF EXISTS user_cloud_access;
-DROP TABLE IF EXISTS user_model_access;
-ALTER TABLE users DROP COLUMN controller_access;
-ALTER TABLE users DROP COLUMN audit_log_access;
-
-UPDATE versions SET major=1, minor=4 WHERE component='jimmdb';
