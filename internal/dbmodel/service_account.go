93ec71714e38e71d27ad9ec046c778fc3ccf65c7
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-01-10 10:30:19 +0000

    Add `ServiceAccount` entity
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

---
+++ internal/dbmodel/service_account.go
@@ -0,0 +1,27 @@
+// Copyright 2020 Canonical Ltd.
+
+package dbmodel
+
+import (
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+type ServiceAccount struct {
+	gorm.Model
+
+	// ClientID is the service account ClientID.
+	ClientID string `gorm:"not null;uniqueIndex"`
+
+	// DisplayName is the name that should be displayed for the service account.
+	DisplayName string `gorm:"not null"`
+
+	// LastLogin is the time the service account last authenticated to the JIMM
+	// server. LastLogin will only be a valid time if the account has
+	// authenticated at least once.
+	LastLogin sql.NullTime
+
+	// CloudCredentials are the cloud credentials owned by this service account.
+	CloudCredentials []CloudCredential `gorm:"foreignKey:OwnerClientID;references:ClientID"`
+}
