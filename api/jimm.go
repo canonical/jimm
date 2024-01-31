6ccd5a78055e1690cac881e86964672bdffb40bc
Kian Parvin <46668016+kian99@users.noreply.github.com>
2024-01-31 11:20:57 +0200

    CSS-6954 list service account command (#1143)
    * Add list cloud-credentials for service accounts
    
    * Added command and test
    
    * Updated wording
    
    * Minor tweaks
    
    * Added json output test
    
    * PR comments

--- api/jimm.go
+++ api/jimm.go
@@ -195,3 +195,10 @@ func (c *Client) MigrateModel(req *params.MigrateModelRequest) (*jujuparams.Init
 func (c *Client) AddServiceAccount(req *params.AddServiceAccountRequest) error {
 	return c.caller.APICall("JIMM", 4, "", "AddServiceAccount", req, nil)
 }
+
+// ListServiceAccountCredentials lists the cloud credentials belonging to a service account.
+func (c *Client) ListServiceAccountCredentials(req *params.ListServiceAccountCredentialsRequest) (*jujuparams.CredentialContentResults, error) {
+	var response jujuparams.CredentialContentResults
+	err := c.caller.APICall("JIMM", 4, "", "ListServiceAccountCredentials", req, &response)
+	return &response, err
+}
