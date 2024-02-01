d5b55a5abef97883f5e36ad6d9577f9e0b479be8
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-02-01 12:25:18 +0000

    Remove unrelated tests
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- cmd/serviceaccounts/cmd/grant_test.go
+++ cmd/serviceaccounts/cmd/grant_test.go
@@ -70,47 +70,6 @@ func (s *grantSuite) TestGrant(c *gc.C) {
 	c.Assert(ok, gc.Equals, true)
 }
 
-func (s *grantSuite) TestNonExistingClientID(c *gc.C) {
-	// alice is superuser
-	bClient := s.UserBakeryClient("alice")
-
-	_, err := cmdtesting.RunCommand(c, cmd.NewGrantCommandForTesting(s.ClientStore(), bClient), "00000000-0000-0000-0000-000000000000", "user-foo")
-
-	// Note that, JIMM currently returns an `unauthorized` error when the given
-	// client ID does not exist. It's because it first looks for an OpenFGA tuple
-	// that relates the user to the requested service account (client ID). Since
-	// the service account does not exist, the tuple is not there as well and
-	// therefore an `unauthorized` error will be returned.
-	c.Assert(err, gc.ErrorMatches, "unauthorized \\(unauthorized access\\)")
-}
-
-func (s *grantSuite) TestUnauthorizedAccess(c *gc.C) {
-	ctx := context.Background()
-
-	clientID := "abda51b2-d735-4794-a8bd-49c506baa4af"
-
-	// alice is superuser
-	bClient := s.UserBakeryClient("alice")
-
-	sa := dbmodel.Identity{
-		Name: clientID,
-	}
-	err := s.JIMM.Database.GetIdentity(ctx, &sa)
-	c.Assert(err, gc.IsNil)
-
-	// Make bob admin of the service account
-	tuple := openfga.Tuple{
-		Object:   ofganames.ConvertTag(names.NewUserTag("bob@external")),
-		Relation: ofganames.AdministratorRelation,
-		Target:   ofganames.ConvertTag(jimmnames.NewServiceAccountTag(clientID)),
-	}
-	err = s.JIMM.OpenFGAClient.AddRelation(ctx, tuple)
-	c.Assert(err, gc.IsNil)
-
-	_, err = cmdtesting.RunCommand(c, cmd.NewGrantCommandForTesting(s.ClientStore(), bClient), clientID, "user-foo")
-	c.Assert(err, gc.ErrorMatches, "unauthorized \\(unauthorized access\\)")
-}
-
 func (s *grantSuite) TestMissingClientIDArg(c *gc.C) {
 	bClient := s.UserBakeryClient("alice")
 	_, err := cmdtesting.RunCommand(c, cmd.NewGrantCommandForTesting(s.ClientStore(), bClient))
