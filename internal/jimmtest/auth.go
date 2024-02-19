6385aed6b31fa3ae9ec570e77aca0a56a222c2c4
Kian Parvin <kian.parvin@canonical.com>
2024-02-19 13:41:36 +0200

    PR changes

--- internal/jimmtest/auth.go
+++ internal/jimmtest/auth.go
@@ -5,13 +5,11 @@ package jimmtest
 import (
 	"context"
 
-	"github.com/coreos/go-oidc/v3/oidc"
 	jujuparams "github.com/juju/juju/rpc/params"
 	"github.com/lestrrat-go/jwx/v2/jwt"
-	"golang.org/x/oauth2"
 
 	"github.com/canonical/jimm/internal/auth"
-	"github.com/canonical/jimm/internal/errors"
+	"github.com/canonical/jimm/internal/jimm"
 	"github.com/canonical/jimm/internal/openfga"
 )
 
@@ -28,6 +26,7 @@ func (a Authenticator) Authenticate(_ context.Context, _ *jujuparams.LoginReques
 }
 
 type MockOAuthAuthenticator struct {
+	jimm.OAuthAuthenticator
 	secretKey string
 }
 
@@ -35,26 +34,8 @@ func NewMockOAuthAuthenticator(secretKey string) MockOAuthAuthenticator {
 	return MockOAuthAuthenticator{secretKey: secretKey}
 }
 
-func (m MockOAuthAuthenticator) Device(ctx context.Context) (*oauth2.DeviceAuthResponse, error) {
-	return nil, errors.E("Device not implemented")
-}
-
-func (m MockOAuthAuthenticator) DeviceAccessToken(ctx context.Context, res *oauth2.DeviceAuthResponse) (*oauth2.Token, error) {
-	return nil, errors.E("DeviceAccessToken not implemented")
-}
-
-func (m MockOAuthAuthenticator) ExtractAndVerifyIDToken(ctx context.Context, oauth2Token *oauth2.Token) (*oidc.IDToken, error) {
-	return nil, errors.E("ExtractAndVerifyIDToken not implemented")
-}
-
-func (m MockOAuthAuthenticator) Email(idToken *oidc.IDToken) (string, error) {
-	return "", errors.E("Email not implemented")
-}
-
-func (m MockOAuthAuthenticator) MintSessionToken(email string, secretKey string) (string, error) {
-	return "", errors.E("MintSessionToken not implemented")
-}
-
+// VerifySessionToken provides the mock implementation for verifying session tokens.
+// Allowing JIMM tests to create their own session tokens that will always be accepted.
 func (m MockOAuthAuthenticator) VerifySessionToken(token string, secretKey string) (jwt.Token, error) {
 	return auth.VerifySessionToken(token, m.secretKey)
 }
