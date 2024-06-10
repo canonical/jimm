c9c56cbf1eae27ab2098b9c59e4748ea6ae826a3
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-06-10 07:24:38 +0100

    Stop session store cleanup job on service shutdown (#1223)
    * Add `Cleanup` method to JIMM service
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Encapsulate session store related steps in its setup method
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Add `AddCleanup` method
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Add test for `Cleanup` method
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Export `Service.cleanups` for testing
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Call `Cleanup` in tests
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Verify cleaning up session-store-related resources does not panic
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Return `error` from cleanup functions
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Add godoc for exported method
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    * Update cleanup function signature
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    
    ---------
    
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>
    Co-authored-by: Kian Parvin <46668016+kian99@users.noreply.github.com>

--- service.go
+++ service.go
@@ -5,7 +5,6 @@ package jimm
 import (
 	"context"
 	"crypto/rand"
-	"database/sql"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -180,7 +179,8 @@ type Params struct {
 type Service struct {
 	jimm jimm.JIMM
 
-	mux *chi.Mux
+	mux      *chi.Mux
+	cleanups []func() error
 }
 
 func (s *Service) JIMM() *jimm.JIMM {
@@ -225,6 +225,22 @@ func (s *Service) StartJWKSRotator(ctx context.Context, checkRotateRequired <-ch
 	return s.jimm.JWKService.StartJWKSRotator(ctx, checkRotateRequired, initialRotateRequiredTime)
 }
 
+// Cleanup cleans up resources that need to be released on shutdown.
+func (s *Service) Cleanup() {
+	// Iterating over clean up function in reverse-order to avoid early clean ups.
+	for i := len(s.cleanups) - 1; i >= 0; i-- {
+		f := s.cleanups[i]
+		if err := f(); err != nil {
+			zapctx.Error(context.Background(), "cleanup failed", zap.Error(err))
+		}
+	}
+}
+
+// AddCleanup adds a clean up function to be run at service shutdown.
+func (s *Service) AddCleanup(f func() error) {
+	s.cleanups = append(s.cleanups, f)
+}
+
 // NewService creates a new Service using the given params.
 func NewService(ctx context.Context, p Params) (*Service, error) {
 	const op = errors.Op("NewService")
@@ -256,10 +272,6 @@ func NewService(ctx context.Context, p Params) (*Service, error) {
 	if err := s.jimm.Database.Migrate(ctx, false); err != nil {
 		return nil, errors.E(op, err)
 	}
-	sqlDb, err := s.jimm.Database.DB.DB()
-	if err != nil {
-		return nil, errors.E(op, err)
-	}
 
 	if p.AuditLogRetentionPeriodInDays != "" {
 		period, err := strconv.Atoi(p.AuditLogRetentionPeriodInDays)
@@ -287,14 +299,10 @@ func NewService(ctx context.Context, p Params) (*Service, error) {
 		return nil, errors.E(op, err)
 	}
 
-	// Setup browser session store
-	sessionStore, err := setupSessionStore(ctx, sqlDb, s.jimm.CredentialStore)
+	sessionStore, err := s.setupSessionStore(ctx)
 	if err != nil {
 		return nil, errors.E(op, err)
 	}
-
-	// Cleanup expired session every 30 minutes
-	sessionStore.Cleanup(time.Minute * 30)
 
 	redirectUrl := p.PublicDNSName + jimmhttp.AuthResourceBasePath + jimmhttp.CallbackEndpoint
 	if !strings.HasPrefix(redirectUrl, "https://") || !strings.HasPrefix(redirectUrl, "http://") {
@@ -418,21 +426,42 @@ func (s *Service) setupDischarger(p Params) (*discharger.MacaroonDischarger, err
 	return MacaroonDischarger, nil
 }
 
-func setupSessionStore(ctx context.Context, db *sql.DB, credStore jimmcreds.CredentialStore) (*pgstore.PGStore, error) {
-	oauthSessionStoreSecret, err := credStore.GetOAuthSessionStoreSecret(ctx)
+func (s *Service) setupSessionStore(ctx context.Context) (*pgstore.PGStore, error) {
+	const op = errors.Op("setupSessionStore")
+
+	if s.jimm.CredentialStore == nil {
+		return nil, errors.E(op, "credential store is not configured")
+	}
+
+	sqlDb, err := s.jimm.Database.DB.DB()
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+
+	oauthSessionStoreSecret, err := s.jimm.CredentialStore.GetOAuthSessionStoreSecret(ctx)
 	if err == nil {
 		zapctx.Info(ctx, "detected existing OAuth session store secret")
 	} else if errors.ErrorCode(err) == errors.CodeNotFound {
-		oauthSessionStoreSecret, err = generateOAuthSessionStoreSecret(ctx, credStore)
+		oauthSessionStoreSecret, err = generateOAuthSessionStoreSecret(ctx, s.jimm.CredentialStore)
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		return nil, errors.E(err, "failed to read session store secret")
+		return nil, errors.E(op, err, "failed to read session store secret")
 	}
 
-	store, err := pgstore.NewPGStoreFromPool(db, oauthSessionStoreSecret)
-	return store, err
+	store, err := pgstore.NewPGStoreFromPool(sqlDb, oauthSessionStoreSecret)
+	if err != nil {
+		return nil, errors.E(op, err, "failed to create session store")
+	}
+
+	// Cleanup expired session every 30 minutes
+	cleanupQuit, cleanupDone := store.Cleanup(time.Minute * 30)
+	s.AddCleanup(func() error {
+		store.StopCleanup(cleanupQuit, cleanupDone)
+		return nil
+	})
+	return store, nil
 }
 
 func generateOAuthSessionStoreSecret(ctx context.Context, store jimmcreds.CredentialStore) ([]byte, error) {
