6f28d847f59b6356e9509d67b564691eaa02e50f
SimoneDutto <simone.dutto@canonical.com>
2024-07-30 16:47:49 +0200

    implement CRUD identities

--- internal/rebac_admin/backend.go
+++ internal/rebac_admin/backend.go
@@ -20,6 +20,7 @@ func SetupBackend(ctx context.Context, jimm *jimm.JIMM) (*rebac_handlers.ReBACAd
 		Authenticator: nil, // Authentication is handled by internal middleware.
 		Entitlements:  newEntitlementService(),
 		Groups:        newGroupService(jimm),
+		Identities:    newidentitiesService(jimm),
 	})
 	if err != nil {
 		zapctx.Error(ctx, "failed to create rebac admin backend", zap.Error(err))
