6c541d8f7f825b7b4328288cc2347f68ab960cd3
Kian Parvin <46668016+kian99@users.noreply.github.com>
2024-08-05 11:29:43 +0200

    rename and remove unused mock (#1302)

--- internal/jimmtest/jimm_mock.go
+++ internal/jimmtest/jimm_mock.go
@@ -64,7 +64,6 @@ type JIMM struct {
 	GetCredentialStore_                func() jimmcreds.CredentialStore
 	GetJimmControllerAccess_           func(ctx context.Context, user *openfga.User, tag names.UserTag) (string, error)
 	GetUser_                           func(ctx context.Context, username string) (*openfga.User, error)
-	GetOpenFGAUserAndAuthorise_        func(ctx context.Context, email string) (*openfga.User, error)
 	GetUserCloudAccess_                func(ctx context.Context, user *openfga.User, cloud names.CloudTag) (string, error)
 	GetUserControllerAccess_           func(ctx context.Context, user *openfga.User, controller names.ControllerTag) (string, error)
 	GetUserModelAccess_                func(ctx context.Context, user *openfga.User, model names.ModelTag) (string, error)
@@ -326,13 +325,7 @@ func (j *JIMM) GetUser(ctx context.Context, username string) (*openfga.User, err
 	if j.GetUser_ == nil {
 		return nil, errors.E(errors.CodeNotImplemented)
 	}
-	return j.GetUser(ctx, username)
-}
-func (j *JIMM) GetOpenFGAUserAndAuthorise(ctx context.Context, email string) (*openfga.User, error) {
-	if j.GetOpenFGAUserAndAuthorise_ == nil {
-		return nil, errors.E(errors.CodeNotImplemented)
-	}
-	return j.GetUser(ctx, email)
+	return j.GetUser_(ctx, username)
 }
 func (j *JIMM) GetUserCloudAccess(ctx context.Context, user *openfga.User, cloud names.CloudTag) (string, error) {
 	if j.GetUserCloudAccess_ == nil {
