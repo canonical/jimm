d9bcceb48ed1bfcf9a03f4704fe8cee233fe0f01
Kian Parvin <kian.parvin@canonical.com>
2024-08-15 11:39:20 +0200

    address comments

--- internal/jujuapi/admin.go
+++ internal/jujuapi/admin.go
@@ -126,7 +126,7 @@ func (r *controllerRoot) LoginWithSessionToken(ctx context.Context, req params.L
 
 	user, err := r.jimm.LoginWithSessionToken(ctx, req.SessionToken)
 	if err != nil {
-		// Avoid replacing the comment. The Juju CLI uses it to determine when to initiate login see [OAuthAuthenticator.VerifySessionToken].
+		// Avoid masking the error code on err below. The Juju CLI uses it to determine when to initiate login see [OAuthAuthenticator.VerifySessionToken].
 		return jujuparams.LoginResult{}, errors.E(op, err)
 	}
 
