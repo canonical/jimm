a2ce27d48a7d8a0ec2953f8fa25c55d17a8e4a32
Kian Parvin <kian.parvin@canonical.com>
2024-09-16 10:22:18 +0200

    use consistent permissions when adding/import models

--- internal/jimm/model.go
+++ internal/jimm/model.go
@@ -661,29 +661,35 @@ func (j *JIMM) AddModel(ctx context.Context, user *openfga.User, args *ModelCrea
 
 	mi := builder.JujuModelInfo()
 
-	if err := j.OpenFGAClient.AddControllerModel(
-		ctx,
-		builder.controller.ResourceTag(),
-		builder.model.ResourceTag(),
-	); err != nil {
+	ownerUser := openfga.NewUser(owner, j.OpenFGAClient)
+	modelTag := names.NewModelTag(mi.UUID)
+	controllerTag := builder.controller.ResourceTag()
+
+	return mi, j.addModelPermissions(ctx, ownerUser, modelTag, controllerTag)
+}
+
+// addModelPermissions grants a user access to a model and sets the relation between the controller and model.
+// Call this when adding/importing a model to set the necessary permissions.
+func (j *JIMM) addModelPermissions(ctx context.Context, owner *openfga.User, mt names.ModelTag, ct names.ControllerTag) error {
+	if err := j.OpenFGAClient.AddControllerModel(ctx, ct, mt); err != nil {
 		zapctx.Error(
 			ctx,
 			"failed to add controller-model relation",
-			zap.String("controller", builder.controller.UUID),
-			zap.String("model", builder.model.UUID.String),
+			zap.String("controller", ct.Id()),
+			zap.String("model", mt.Id()),
 		)
+		return err
 	}
-	err = openfga.NewUser(owner, j.OpenFGAClient).SetModelAccess(ctx, names.NewModelTag(mi.UUID), ofganames.AdministratorRelation)
-	if err != nil {
+	if err := owner.SetModelAccess(ctx, mt, ofganames.AdministratorRelation); err != nil {
 		zapctx.Error(
 			ctx,
 			"failed to add administrator relation",
-			zap.String("user", owner.Tag().String()),
-			zap.String("model", builder.model.UUID.String),
+			zap.String("user", owner.Tag().Id()),
+			zap.String("model", mt.Id()),
 		)
+		return err
 	}
-
-	return mi, nil
+	return nil
 }
 
 // ModelInfo returns the model info for the model with the given ModelTag.
