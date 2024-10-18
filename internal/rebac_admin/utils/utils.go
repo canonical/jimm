40deb5d22c11d89c1d01bf17c7667c4719f3ba86
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-10-18 12:05:00 +0100

    Return identity name as identifier
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- internal/rebac_admin/utils/utils.go
+++ internal/rebac_admin/utils/utils.go
@@ -3,7 +3,6 @@
 package utils
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/canonical/rebac-admin-ui-handlers/v1/resources"
@@ -17,12 +16,11 @@ import (
 
 // FromUserToIdentity parses openfga.User into resources.Identity .
 func FromUserToIdentity(user openfga.User) resources.Identity {
-	id := fmt.Sprintf("%d", user.ID)
 	joined := user.CreatedAt.Format(time.RFC3339)
 	lastLogin := user.LastLogin.Time.Format(time.RFC3339)
 	return resources.Identity{
 		Email:     user.Name,
-		Id:        &id,
+		Id:        &user.Name,
 		Joined:    &joined,
 		LastLogin: &lastLogin,
 		Source:    "",
