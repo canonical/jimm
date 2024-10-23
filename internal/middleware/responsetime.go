a5f1e72830a7c87d61dbf537aaf4655f865ab292
SimoneDutto <simone.dutto@canonical.com>
2024-10-23 11:31:00 +0200

    track status code and use route pattern

--- internal/middleware/responsetime.go
+++ internal/middleware/responsetime.go
@@ -4,21 +4,40 @@ package middleware
 
 import (
 	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/go-chi/chi/v5"
 
 	"github.com/canonical/jimm/v3/internal/servermon"
 )
+
+// statusRecorder to record the status code from the ResponseWriter
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rec *statusRecorder) WriteHeader(statusCode int) {
+	rec.statusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
 
 // MeasureResponseTime tracks response time of requests.
 func MeasureResponseTime(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Check the upgrade header because we only track http endpoints
-		if r.Header.Get("Upgrade") != "websocket" {
-			start := time.Now()
-			defer func() {
-				servermon.ResponseTimeHistogram.WithLabelValues(r.URL.Path, r.Method).Observe(time.Since(start).Seconds())
-			}()
+		if r.Header.Get("Upgrade") == "websocket" {
+			next.ServeHTTP(w, r)
+			return
 		}
-		next.ServeHTTP(w, r)
+		rec := statusRecorder{w, 200}
+		start := time.Now()
+		defer func() {
+			route := chi.RouteContext(r.Context()).RoutePattern()
+			statusCode := strconv.Itoa(rec.statusCode)
+			servermon.ResponseTimeHistogram.WithLabelValues(route, r.Method, statusCode).Observe(time.Since(start).Seconds())
+		}()
+		next.ServeHTTP(&rec, r)
 	})
 }
--- internal/servermon/monitoring.go
+++ internal/servermon/monitoring.go
@@ -136,7 +136,7 @@ var (
 		Name:      "http",
 		Help:      "request_duration_seconds",
 		Buckets:   []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
-	}, []string{"route", "method"})
+	}, []string{"route", "method", "status_code"})
 )
 
 // DurationObserver returns a function that, when run with `defer` will
