aaba0bcdd6763a0b5b4638f71672b1b3b3271999
Kian Parvin <kian.parvin@canonical.com>
2024-12-02 10:19:42 +0200

    chore: create group manager in app layer unit tests

--- internal/jimm/service_account_test.go
+++ internal/jimm/service_account_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/canonical/jimm/v3/internal/db"
 	"github.com/canonical/jimm/v3/internal/dbmodel"
 	"github.com/canonical/jimm/v3/internal/jimm"
+	"github.com/canonical/jimm/v3/internal/jimm/group"
 	"github.com/canonical/jimm/v3/internal/openfga"
 	ofganames "github.com/canonical/jimm/v3/internal/openfga/names"
 	"github.com/canonical/jimm/v3/internal/testutils/jimmtest"
@@ -240,9 +241,12 @@ func TestGrantServiceAccountAccess(t *testing.T) {
 			}
 			err = pgDb.Migrate(context.Background(), false)
 			c.Assert(err, qt.IsNil)
+			groupManager, err := group.NewGroupManager(&pgDb, ofgaClient)
+			c.Assert(err, qt.IsNil)
 			jimm := &jimm.JIMM{
 				Database:      pgDb,
 				OpenFGAClient: ofgaClient,
+				GroupManager:  groupManager,
 			}
 			var u dbmodel.Identity
 			u.SetTag(names.NewUserTag(test.clientID))
