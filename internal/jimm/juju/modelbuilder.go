0b908fac895f0398012987a57928a6daef43dcce
Kian Parvin <kian.parvin@canonical.com>
2025-04-02 12:24:26 +0200

    fix: missing check for cloud-credential owner

--- internal/jimm/juju/modelbuilder.go
+++ internal/jimm/juju/modelbuilder.go
@@ -227,6 +227,13 @@ func (b *modelBuilder) WithCloudCredential(credentialTag names.CloudCredentialTa
 	if b.err != nil {
 		return b
 	}
+
+	// Verify ownership of cloud credential
+	if b.owner == nil || b.owner.Name != credentialTag.Owner().Id() {
+		b.err = errors.E("invalid owner for cloud credential", errors.CodeUnauthorized)
+		return b
+	}
+
 	credential := dbmodel.CloudCredential{
 		Name:              credentialTag.Name(),
 		CloudName:         credentialTag.Cloud().Id(),
