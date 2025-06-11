283c1bc1547b464762bf624d988e26fd4e31a6e6
ale8k <alexjkilroy@gmail.com>
2025-06-11 11:10:43 +0100

    feat: prepareModelMigration method
    This method prepares JIMM for an incoming model migration. It stores the model uuid, target
    controller name and usermap, to be referred to later when the migrate methods require these details.

--- internal/jimm/juju/jimm.go
+++ internal/jimm/juju/jimm.go
@@ -10,6 +10,8 @@ import (
 	"github.com/google/uuid"
 	jujuparams "github.com/juju/juju/rpc/params"
 	"github.com/juju/names/v5"
+	"github.com/juju/zaputil/zapctx"
+	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/canonical/jimm/v3/internal/db"
@@ -266,4 +268,37 @@ func (j *JujuManager) InitiateInternalMigration(ctx context.Context, user *openf
 		return result, errors.E(op, err)
 	}
 	return result, nil
+}
+
+// PrepareModelMigration takes the model ID from the migrating controller and stores a record
+// in the IncomingModelMigation table to prepare it for migration against the target controller's name.
+func (j *JujuManager) PrepareModelMigration(
+	ctx context.Context,
+	user *openfga.User,
+	modelUUID string,
+	targetControllerName string,
+	userMapping []byte,
+) error {
+	const op = errors.Op("jujumanager.PrepareModelMigration")
+
+	// TODO(ale8k): Use transaction here, to get ctl & then add the model migration
+	// Problem is our DB methods don't take a db object and instead use the instance on
+	// the db struct. Perhaps create two new methods with "TX" in the name...?
+	// Will do this in a follow up.
+	ctl, err := j.getControllerByName(ctx, targetControllerName)
+	if err != nil {
+		zapctx.Error(ctx, "failed to get controller", zap.Error(err))
+		return errors.E(op, err)
+	}
+
+	if err := j.Database.AddIncomingModelMigration(ctx, &dbmodel.IncomingModelMigration{
+		ModelUUID:          sql.NullString{String: modelUUID, Valid: true},
+		TargetControllerID: ctl.ID,
+		UserMapping:        dbmodel.JSON(userMapping),
+	}); err != nil {
+		zapctx.Error(ctx, "failed to add incoming model migration details", zap.Error(err))
+		return errors.E(op, err)
+	}
+
+	return nil
 }
--- internal/jimm/juju/jimm_test.go
+++ internal/jimm/juju/jimm_test.go
@@ -694,7 +694,6 @@ func TestInitiateInternalMigration(t *testing.T) {
 
 			dbUser := env.User(test.user).DBObject(c, j.Database)
 			user := openfga.NewUser(&dbUser, nil)
-			c.Assert(err, qt.IsNil)
 
 			res, err := j.InitiateInternalMigration(
 				ctx,
@@ -710,4 +709,108 @@ func TestInitiateInternalMigration(t *testing.T) {
 			}
 		})
 	}
+}
+
+const PrepareModelMigrationTestEnv = `clouds:
+- name: test-cloud
+  type: test-provider
+  regions:
+  - name: test-cloud-region
+cloud-credentials:
+  - owner: alice@canonical.com
+    name: cred-1
+    cloud: test-cloud
+controllers:
+- name: myController
+  uuid: 00000001-0000-0000-0000-000000000001
+  cloud: test-cloud
+  region: test-cloud-region
+- name: myController2
+  uuid: 00000002-0000-0000-0000-000000000002
+  cloud: test-cloud
+  region: test-cloud-region
+models:
+  - name: model-1
+    uuid: 00000002-0000-0000-0000-000000000001
+    controller: myController
+    cloud: test-cloud
+    region: test-cloud-region
+    cloud-credential: cred-1
+    owner: alice@canonical.com
+    life: alive
+users:
+  - username: alice@canonical.com
+    controller-access: superuser
+`
+
+func TestPrepareModelMigration_ControllerDoesNotExist(t *testing.T) {
+	c := qt.New(t)
+	ctx := c.Context()
+
+	store := jimmtest.NewInMemoryCredentialStore()
+	j := newTestJujuManager(c, &parameters{
+		CredentialStore: store,
+	})
+
+	env := jimmtest.ParseEnvironment(c, PrepareModelMigrationTestEnv)
+	// We delete the controller from the env, and check for this UUID
+	targetControllerName := env.Controllers[1].Name
+	env.Controllers = env.Controllers[:1]
+	env.PopulateDB(c, j.Database)
+
+	dbUser := env.User("alice@canonical.com").DBObject(c, j.Database)
+	user := openfga.NewUser(&dbUser, nil)
+
+	user.JimmAdmin = true
+
+	userMapping := []byte(`{"alice":"alice@canonical.com"}`)
+
+	err := j.PrepareModelMigration(
+		ctx,
+		user,
+		env.Models[0].DBObject(c, j.Database).UUID.String,
+		targetControllerName,
+		userMapping,
+	)
+	c.Assert(err, qt.ErrorMatches, "controller not found")
+}
+
+func TestPrepareModelMigration_Success(t *testing.T) {
+	c := qt.New(t)
+	ctx := c.Context()
+
+	store := jimmtest.NewInMemoryCredentialStore()
+	j := newTestJujuManager(c, &parameters{
+		CredentialStore: store,
+	})
+
+	env := jimmtest.ParseEnvironment(c, PrepareModelMigrationTestEnv)
+	env.PopulateDB(c, j.Database)
+	dbUser := env.User("alice@canonical.com").DBObject(c, j.Database)
+	user := openfga.NewUser(&dbUser, nil)
+	user.JimmAdmin = true
+
+	modelUUID := env.Models[0].DBObject(c, j.Database).UUID.String
+	userMapping := []byte(`{"alice": "alice@canonical.com"}`)
+	targetController := env.Controllers[1].DBObject(c, j.Database)
+
+	err := j.PrepareModelMigration(
+		ctx,
+		user,
+		modelUUID,
+		targetController.Name,
+		userMapping,
+	)
+	c.Assert(err, qt.IsNil)
+
+	incomingMigration := &dbmodel.IncomingModelMigration{
+		ModelUUID:          sql.NullString{String: modelUUID, Valid: true},
+		TargetControllerID: targetController.ID,
+	}
+	err = j.Database.GetIncomingModelMigration(ctx, incomingMigration)
+	c.Assert(err, qt.IsNil)
+
+	c.Assert(incomingMigration.ModelUUID.String, qt.Equals, modelUUID)
+	c.Assert(incomingMigration.TargetController.UUID, qt.Equals, targetController.UUID)
+	c.Assert(incomingMigration.UserMapping, qt.DeepEquals, dbmodel.JSON(userMapping))
 }
