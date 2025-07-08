70df2a12d186da216ee918545e14b209cf2dce19
ale8k <alexjkilroy@gmail.com>
2025-07-08 10:03:09 +0100

    feat: bootstrap cmd implementation

--- internal/jujucommands/bootstrap.go
+++ internal/jujucommands/bootstrap.go
@@ -7,16 +7,20 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 
-	"github.com/juju/juju/cloud"
+	jujucloud "github.com/juju/juju/cloud"
+	"github.com/juju/juju/jujuclient"
+	_ "github.com/juju/juju/provider/lxd"
 	"github.com/juju/version/v2"
-
-	"github.com/canonical/jimm/v3/internal/openfga"
+	"github.com/juju/zaputil/zapctx"
+	"go.uber.org/zap"
 )
 
+// BootstrapCmdParams holds the parameters to bootstrap a controller for JIMM.
 type BootstrapCmdParams struct {
-	CloudName            string
+	CloudNameAndRegion   string
 	ControllerName       string
 	AgentVersion         string
 	BootstrapTimeout     int
@@ -24,8 +28,8 @@ type BootstrapCmdParams struct {
 }
 
 func (b BootstrapCmdParams) validate() error {
-	if b.CloudName == "" {
-		return errors.New("cloud name cannot be empty")
+	if b.CloudNameAndRegion == "" {
+		return errors.New("cloud [and region] name cannot be empty")
 	}
 
 	if b.ControllerName == "" {
@@ -69,46 +73,143 @@ func (b BootstrapCmdParams) buildBootstrapCmdStr() string {
 	}
 
 	// Always add controller name & cloud at the end
-	builder.WriteString(fmt.Sprintf(" %s", b.CloudName))
+	builder.WriteString(fmt.Sprintf(" %s", b.CloudNameAndRegion))
 	builder.WriteString(fmt.Sprintf(" %s", b.ControllerName))
 	return builder.String()
 }
 
+// RunBootstrapCmd enables the caller to a bootstrap a controller that is ready to be added
+// to JIMM. The caller may specify just a credential and empty personal cloud if the target
+// cloud is a known public cloud. If it isn't, the personal cloud must be correctly populated.
+//
+// It returns a output channel which is closed once the command completes. Additionally,
+// it returns a closure which cleans up the temporary $JUJU_DATA directory created for the
+// lifetime of this command.
 func RunBootstrapCmd(
 	ctx context.Context,
-	u *openfga.User,
 	p BootstrapCmdParams,
-	cred cloud.Credential,
-) (<-chan outputLine, error) {
+	personalCloud jujucloud.Cloud,
+	cred jujucloud.CloudCredential,
+	pubKey []byte,
+	privKey []byte,
+) (<-chan outputLine, func(), error) {
 	if err := p.validate(); err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	// memStore := jujuclient.NewMemStore()
-	// Update public clouds and set JUJU_HOME
-	os.Setenv("JUJU_HOME", "./")
+	memStore := jujuclient.NewMemStore()
 
-	// 1. Create clouds.yaml if cloud isn't a public cloud
-	// TODO: Create temp file and set JUJU_HOME and make a todo about how mem store gotta model clouds
-	// so we can run multiple bootstraps, cause we currently gotta use JUJU_HOME and clouds.yaml (PersonalCLoud w/e it called)
-	// will be location then delete it after
+	// Setup JUJU_DATA
+	tmpJujuData, err := os.MkdirTemp("", "juju-data-*")
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create temp JUJU_DATA: %w", err)
+	}
 
-	// 2. Add credential to mem store
+	zapctx.Debug(ctx, "Setting JUJU_DATA path", zap.String("path", tmpJujuData))
+	os.Setenv("JUJU_DATA", tmpJujuData)
+	// This didn't work... Need to figure a way to not use env var.
+	// if err := cmdCtx.Setenv("JUJU_DATA", tmpJujuData); err != nil {
+	// 	return nil, nil, err
+	// }
 
-	// Add cloud call looks like:
-	// func (c *Client) AddCloud(cloud jujucloud.Cloud, force bool) error {
-	// args := params.AddCloudArgs{Name: cloud.Name, Cloud: cloudToParams(cloud)}
-	// if force {
+	// Setup SSH keys
+	// TODO: This isn't working, it's using hosts SSH keys (how do I specify this?)
+	// sshDir := filepath.Join(tmpJujuData, "ssh")
+	// if err := os.MkdirAll(sshDir, 0700); err != nil {
+	// 	return nil, nil, fmt.Errorf("failed to create .ssh directory: %w", err)
+	// }
 
-	// cloud.Cloud{}
-	// memStore.UpdateCredential(p.CloudName)
+	// if err := os.WriteFile(sshDir+"/juju_id_rsa.pub", []byte(pubKey), 0600); err != nil {
+	// 	return nil, nil, fmt.Errorf("writing public key failed: %w", err)
+	// }
 
-	// Send cloud and creds on facade
-	// Call bootstrap with them in memory
-	// Add controller to db
-	// Add cloud to db (retrieve it from controller)
-	// Add credential to db
+	// if err = os.WriteFile(sshDir+"/juju_id_rsa", []byte(privKey), 0600); err != nil {
+	// 	return nil, nil, fmt.Errorf("writing private key failed: %w", err)
+	// }
 
-	return nil, nil
-	// return runCmdWithOutputRetriever(memStore, cmdStr)
+	// Update public clouds
+	// TODO: Make this a command of this package
+	outputCh, err := runCmdWithOutputRetriever(memStore, "update-public-clouds --client")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for line := range outputCh {
+		if line.Err != nil {
+			return nil, nil, fmt.Errorf("failed to update public clouds: %w", err)
+		}
+	}
+
+	// Check if we can get the cloud as a public cloud.
+	cloudName, regionName := splitCloudNameAndRegion(p.CloudNameAndRegion)
+	isAPublicCloud, err := isAValidPublicCloud(cloudName, regionName)
+	if err != nil {
+		return nil, nil, err
+	}
+	if !isAPublicCloud {
+		// We presume it is a personal cloud
+		// TODO: Check if credential should be cloudname or include region
+		if err := jujucloud.WritePersonalCloudMetadata(map[string]jujucloud.Cloud{
+			cloudName: personalCloud,
+		}); err != nil {
+			return nil, nil, fmt.Errorf("failed to write personal cloud: %w", err)
+		}
+	}
+
+	// TODO: check if cloudName should include region, presuming not right now
+	if err := memStore.UpdateCredential(cloudName, cred); err != nil {
+		return nil, nil, fmt.Errorf("failed to set credential: %w", err)
+	}
+
+	// With the clouds set, credentials updated, we now bootstrap.
+	cmdStr := p.buildBootstrapCmdStr()
+
+	cleanupTmpJujuData := func() {
+		os.Unsetenv("JUJU_DATA")
+		os.RemoveAll(tmpJujuData)
+	}
+
+	outputRetriever, err := runCmdWithOutputRetriever(memStore, cmdStr)
+	return outputRetriever, cleanupTmpJujuData, err
+}
+
+// isAValidPublicCloud checks if the cloud name (and possibly region) is a valid
+// public cloud and region. If it is a public cloud without the region specified,
+// just the cloud name is checked. If a region is specified, but it isn't valid,
+// an error is returned.
+//
+// TODO: Is there a better way to do this? (Rather than look up metadata and loop through)
+func isAValidPublicCloud(cloudName, regionName string) (bool, error) {
+	var isAPublicCloud bool
+
+	pubClouds, _, err := jujucloud.PublicCloudMetadata(jujucloud.JujuPublicCloudsPath())
+	if err != nil {
+		return false, fmt.Errorf("failed to get public cloud metadata: %w", err)
+	}
+
+	for pubCloudName, cloud := range pubClouds {
+		if cloudName == pubCloudName {
+			isAPublicCloud = true
+			if regionName != "" {
+				exists := slices.ContainsFunc(cloud.Regions, func(r jujucloud.Region) bool {
+					return regionName == r.Name
+				})
+				if !exists {
+					return false, fmt.Errorf("invalid public cloud region for cloud %s with region %s", cloudName, regionName)
+				}
+			}
+		}
+	}
+
+	return isAPublicCloud, nil
+}
+
+func splitCloudNameAndRegion(cloudNameAndRegion string) (cloudName string, regionName string) {
+	if i := strings.IndexRune(cloudNameAndRegion, '/'); i > 0 {
+		cloudName, regionName = cloudNameAndRegion[:i], cloudNameAndRegion[i+1:]
+	} else {
+		cloudName = cloudNameAndRegion
+	}
+
+	return
 }
