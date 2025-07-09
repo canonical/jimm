e98a7fce1c44ee362dafc34fdc727cd95439dd69
Kian Parvin <kian.parvin@canonical.com>
2025-07-09 12:29:51 +0200

    chore: remove user mappings on model removal

--- internal/dbmodel/usermapping_test.go
+++ internal/dbmodel/usermapping_test.go
@@ -18,27 +18,93 @@ func TestUserMappingUniqueConstraint(t *testing.T) {
 	c := qt.New(t)
 	db := gormDB(c)
 
-	u, err := dbmodel.NewIdentity("bob@canonical.com")
-	c.Assert(err, qt.IsNil)
-	c.Assert(db.Create(&u).Error, qt.IsNil)
+	cl, cred, ctl, u := initModelEnv(c, db)
 
-	m := dbmodel.UserMapping{
-		ModelUUID: sql.NullString{
+	model := dbmodel.Model{
+		UUID: sql.NullString{
 			String: "00000001-0000-0000-0000-000000000001",
 			Valid:  true,
 		},
+		Owner:           u,
+		Name:            "test-1",
+		Controller:      ctl,
+		CloudRegion:     cl.Regions[0],
+		CloudCredential: cred,
+	}
+	c.Assert(db.Create(&model).Error, qt.IsNil)
+
+	m := dbmodel.UserMapping{
+		ModelUUID:        model.UUID,
 		LocalUser:        "bob",
 		ExternalUserName: u.Name,
 	}
 	c.Assert(db.Create(&m).Error, qt.IsNil)
 
 	m2 := dbmodel.UserMapping{
-		ModelUUID: sql.NullString{
-			String: "00000001-0000-0000-0000-000000000001",
-			Valid:  true,
-		},
+		ModelUUID:        model.UUID,
 		LocalUser:        "bob",
 		ExternalUserName: u.Name,
 	}
 	c.Assert(db.Create(&m2).Error, qt.ErrorMatches, ".*duplicate key value violates unique constraint \"unique_user_mappings_key\".*")
+}
+
+func TestUserMappingModelUUIDNotEmpty(t *testing.T) {
+	c := qt.New(t)
+	db := gormDB(c)
+
+	cl, cred, ctl, u := initModelEnv(c, db)
+
+	// Create a model without a model UUID,
+	// e.g. we are waiting for Juju to provide us the UUID.
+	model := dbmodel.Model{
+		Owner:           u,
+		Name:            "test-1",
+		Controller:      ctl,
+		CloudRegion:     cl.Regions[0],
+		CloudCredential: cred,
+	}
+	c.Assert(db.Create(&model).Error, qt.IsNil)
+
+	m := dbmodel.UserMapping{
+		ModelUUID:        model.UUID,
+		LocalUser:        "bob",
+		ExternalUserName: u.Name,
+	}
+	c.Assert(db.Create(&m).Error, qt.IsNotNil)
+}
+
+func TestUserMappingIsDeletedWhenModelIsDeleted(t *testing.T) {
+	c := qt.New(t)
+	db := gormDB(c)
+
+	cl, cred, ctl, u := initModelEnv(c, db)
+
+	model := dbmodel.Model{
+		UUID: sql.NullString{
+			String: "00000001-0000-0000-0000-000000000001",
+			Valid:  true,
+		},
+		Owner:           u,
+		Name:            "test-1",
+		Controller:      ctl,
+		CloudRegion:     cl.Regions[0],
+		CloudCredential: cred,
+	}
+	c.Assert(db.Create(&model).Error, qt.IsNil)
+
+	m := dbmodel.UserMapping{
+		ModelUUID:        model.UUID,
+		LocalUser:        "bob",
+		ExternalUserName: u.Name,
+	}
+	c.Assert(db.Create(&m).Error, qt.IsNil)
+
+	var count int64
+	c.Assert(db.Model(&dbmodel.UserMapping{}).Where("model_uuid = ?", model.UUID).Count(&count).Error, qt.IsNil)
+	c.Assert(count, qt.Equals, int64(1))
+
+	c.Assert(db.Delete(&model).Error, qt.IsNil)
+
+	c.Assert(db.Model(&dbmodel.UserMapping{}).Where("model_uuid = ?", model.UUID).Count(&count).Error, qt.IsNil)
+	c.Assert(count, qt.Equals, int64(0))
 }
