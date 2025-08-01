a1f9c5b3f0cf11e1f107e331d2d9ed76142fd01f
Kian Parvin <kian.parvin@canonical.com>
2025-08-01 15:08:26 +0200

    chore: add test for scenario with no users with access to model

--- internal/jimm/juju/migrationtarget_test.go
+++ internal/jimm/juju/migrationtarget_test.go
@@ -224,6 +224,59 @@ func TestControllerDetailsForIncomingModel(t *testing.T) {
 	c.Assert(controllerDetails.Credentials.AdminPassword, qt.Equals, "test-password")
 }
 
+func TestPreChecks_NoUsersWithAccess(t *testing.T) {
+	c := qt.New(t)
+	ctx := context.Background()
+
+	api := &jimmtest.API{
+		Prechecks_: func(mi migration.ModelInfo) error {
+			return nil
+		},
+	}
+
+	j := newTestJujuManager(c, &parameters{
+		Dialer: &jimmtest.Dialer{
+			API: api,
+		},
+	})
+
+	env := jimmtest.ParseEnvironment(c, testEnvWithIncomingMigration)
+	env.PopulateDBAndPermissions(c, j.ResourceTag(), j.Database, j.OpenFGAClient)
+
+	dbUser := env.User("alice@canonical.com").DBObject(c, j.Database)
+	user := openfga.NewUser(&dbUser, j.OpenFGAClient)
+
+	// Below is a simple model description with no users that have access.
+	descriptionArgs := description.ModelArgs{
+		AgentVersion: "3.2.1",
+		Owner:        names.NewUserTag("bob"),
+		Type:         description.IAAS,
+		Cloud:        "test",
+		Config: map[string]interface{}{
+			"uuid": migratingModelUUID,
+			"name": "test-model",
+		},
+		CloudRegion: "test-region",
+	}
+	modelDescription := description.NewModel(descriptionArgs)
+
+	modelDescription.SetCloudCredential(description.CloudCredentialArgs{
+		Owner: names.NewUserTag("bob"),
+		Name:  "test-cred",
+		Cloud: names.NewCloudTag("test"),
+	})
+	modelInfo := migration.ModelInfo{
+		UUID:                   migratingModelUUID,
+		Owner:                  names.NewUserTag("bob"),
+		Name:                   "test-model",
+		AgentVersion:           version.MustParse("3.2.1"),
+		ControllerAgentVersion: version.MustParse("3.2.1"),
+		ModelDescription:       modelDescription,
+	}
+	err := j.Prechecks(ctx, user, modelInfo)
+	c.Assert(err, qt.IsNil)
+}
+
 func TestPreChecks_ValidatesUserMapping(t *testing.T) {
 	c := qt.New(t)
 	ctx := context.Background()
