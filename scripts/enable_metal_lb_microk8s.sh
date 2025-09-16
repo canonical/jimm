edeea6260a6502116330eaac919333a77f6e31b0
ale8k <alexjkilroy@gmail.com>
2025-09-16 11:30:03 +0100

    .

--- scripts/enable_metal_lb_microk8s.sh
+++ scripts/enable_metal_lb_microk8s.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-
-sudo microk8s enable metallb:50.50.50.2/24
+# This script enables MetalLB on MicroK8s with a specified subnet.
+subnet="$(ip route get 1 | head -n 1 | awk '{print $7}' | awk -F. '{print $1 "." $2 "." $3 ".240/24"}')"
+echo "MetalLB subnet: $subnet"
+sudo microk8s enable metallb:"$subnet"
