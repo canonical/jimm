146f43768cc2804e77654523f95953247b6bfd13
ale8k <alexjkilroy@gmail.com>
2025-11-26 13:20:10 +0000

    feat: implement upgrade-to method for automated upgrades
    feat: expose Dial via JujuManager
    
    Exposing dial via the juju manager gives us a singular place to reliably dial controllers. It is
    reasonable to expect the juju manager is capable of dialing controllers, hence my choice not to
    re-embed the dialer.
    
    feat: expose GetControllerByName
    
    Exposes get controller by name as to not duplicate logic.
    
    feat: plugin exposed methods
    
    refactor: revert dial
    
    refactor: plugin dialer and store
    
    refactor: revert permission and getControllerByName public exports
    
    refactor: revert facade code
    
    docs: godoc manager interfaces
    
    refactor: slim api mock
    
    refactor: address PR comments
    
    refactor: use internal jujuclient in migrate and upgrade call
    
    feat: adds the upgrade to method
    
    test: add happy path for UpgradeTo
    
    A simple happy path for upgradeto method. Simply verifies the name is set as expected and the chosen
    version is surfaced.
    
    refactor: rebase v3 in
    
    test: refactor TestMigrateAndUpgradeModel_Success
    
    Refactors TestMigrateAndUpgradeModel_Success to be more explicit in DoAndReturns, verifying the
    parameters are as expected.

--- internal/jujuapi/jimm.go
+++ internal/jujuapi/jimm.go
@@ -15,7 +15,6 @@ import (
 	"github.com/juju/juju/core/network"
 	jujuparams "github.com/juju/juju/rpc/params"
 	"github.com/juju/names/v5"
-	jujuversion "github.com/juju/version/v2"
 
 	"github.com/canonical/jimm/v3/internal/db"
 	"github.com/canonical/jimm/v3/internal/dbmodel"
