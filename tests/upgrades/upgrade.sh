77fb0e7f9b4e2e508072abb58624bd6b377679c0
ale8k <alexjkilroy@gmail.com>
2025-12-05 14:23:58 +0000

    ci: update upgrade.sh to verify only controller model remains and new model has been upgraded

--- tests/upgrades/upgrade.sh
+++ tests/upgrades/upgrade.sh
@@ -7,13 +7,13 @@ set -euo pipefail
 source "local/jimm/detect-jaas.sh"
 
 # Reset Juju on EXIT.
-trap 'snap refresh juju --channel=3/stable' EXIT
+trap 'sudo snap refresh juju --channel=3/stable' EXIT
 
 export CONTROLLER_NAME="man-of-iron"
 JIMM_CONTROLLER_NAME="${JIMM_CONTROLLER_NAME:-jimm-dev}"
 
 # Switch to an older Juju temporarily, so that we can upgrade the model when migrated.
-snap refresh juju --channel=3/stable --revision=32912
+sudo snap refresh juju --channel=3/stable --revision=32912
 
 echo
 echo "Bootstrapping lxd controller configured with login-token-refresh-url"
@@ -27,3 +27,24 @@ echo
 echo "Upgrading Juju 3.6.11 model to 3.6.12"
 $JAAS upgrade-to 3.6.12 $(juju show-model test-lxd | yq '.test-lxd.model-uuid')
 
+echo 
+echo "Verifying model has been migrated from the original controller"
+juju switch man-of-iron
+if jujy models --format json | jq -e '.models | (length == 1) and (.[0].name == "admin/controller")'; then
+  echo "Success: Only the controller model remains."
+else
+  echo "Failure: Expected only the controller model, but found something else."
+  juju models
+  exit 1
+fi
+
+echo
+echo "Verifying model has been upgraded from 3.6.11"
+juju switch $JIMM_CONTROLLER_NAME:test-lxd
+if juju status --format json | jq -e '.model.version != "3.6.11"'; then
+  echo "Validation successful: The model version is not 3.6.11."
+  # Script continues...
+else
+  echo "Validation failed: The model version is still 3.6.11."
+  exit 1
+fi
