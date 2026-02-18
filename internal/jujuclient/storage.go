5000be66fca9409b664c790f6bf5d74d4e74be7c
ale8k <alexjkilroy@gmail.com>
2026-02-18 12:22:13 +0000

    feat: use juju client for storage

--- internal/jujuclient/storage.go
+++ internal/jujuclient/storage.go
@@ -5,100 +5,23 @@ package jujuclient
 import (
 	"context"
 
-	jujuerrors "github.com/juju/errors"
+	"github.com/juju/juju/api/client/storage"
 	jujuparams "github.com/juju/juju/rpc/params"
-	"github.com/juju/names/v5"
-
-	"github.com/canonical/jimm/v3/internal/errors"
 )
 
 // ListFilesystems lists filesystems for desired machines.
 // If no machines provided, a list of all filesystems is returned.
 func (c Connection) ListFilesystems(ctx context.Context, machines []string) ([]jujuparams.FilesystemDetailsListResult, error) {
-
-	filters := make([]jujuparams.FilesystemFilter, len(machines))
-	for i, machine := range machines {
-		filters[i].Machines = []string{names.NewMachineTag(machine).String()}
-	}
-	if len(filters) == 0 {
-		filters = []jujuparams.FilesystemFilter{{}}
-	}
-
-	args := jujuparams.FilesystemFilters{
-		Filters: filters,
-	}
-	var results jujuparams.FilesystemDetailsListResults
-
-	if err := c.CallHighestFacadeVersion(ctx, "Storage", []int{6}, "", "ListFilesystems", &args, &results); err != nil {
-		return nil, errors.E(jujuerrors.Cause(err))
-	}
-
-	if len(results.Results) != len(filters) {
-		return nil, errors.E(
-			jujuerrors.Errorf(
-				"expected %d result(s), got %d",
-				len(filters), len(results.Results),
-			),
-		)
-	}
-
-	return results.Results, nil
+	return storage.NewClient(&c).ListFilesystems(machines)
 }
 
 // ListVolumes lists volumes for desired machines.
 // If no machines provided, a list of all volumes is returned.
 func (c Connection) ListVolumes(ctx context.Context, machines []string) ([]jujuparams.VolumeDetailsListResult, error) {
-
-	filters := make([]jujuparams.VolumeFilter, len(machines))
-	for i, machine := range machines {
-		filters[i].Machines = []string{names.NewMachineTag(machine).String()}
-	}
-	if len(filters) == 0 {
-		filters = []jujuparams.VolumeFilter{{}}
-	}
-	args := jujuparams.VolumeFilters{Filters: filters}
-	var results jujuparams.VolumeDetailsListResults
-
-	if err := c.CallHighestFacadeVersion(ctx, "Storage", []int{6}, "", "ListVolumes", &args, &results); err != nil {
-		return nil, errors.E(jujuerrors.Cause(err))
-	}
-
-	if len(results.Results) != len(filters) {
-		return nil, errors.E(
-			jujuerrors.Errorf(
-				"expected %d result(s), got %d",
-				len(filters), len(results.Results),
-			),
-		)
-	}
-
-	return results.Results, nil
+	return storage.NewClient(&c).ListVolumes(machines)
 }
 
 // ListStorageDetails lists all storage.
 func (c Connection) ListStorageDetails(ctx context.Context) ([]jujuparams.StorageDetails, error) {
-
-	args := jujuparams.StorageFilters{
-		Filters: []jujuparams.StorageFilter{{}}, // one empty filter
-	}
-	var results jujuparams.StorageDetailsListResults
-
-	if err := c.CallHighestFacadeVersion(ctx, "Storage", []int{6}, "", "ListStorageDetails", &args, &results); err != nil {
-		return nil, errors.E(jujuerrors.Cause(err))
-	}
-
-	if len(results.Results) != 1 {
-		return nil, errors.E(
-			jujuerrors.Errorf(
-				"expected 1 result, got %d",
-				len(results.Results),
-			),
-		)
-	}
-	if results.Results[0].Error != nil {
-		return nil, errors.E(
-			jujuerrors.Trace(results.Results[0].Error),
-		)
-	}
-	return results.Results[0].Result, nil
+	return storage.NewClient(&c).ListStorageDetails()
 }
