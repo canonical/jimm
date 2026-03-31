3560b020b25060fbc2b83028e6309042ba73f146
Kian Parvin <kian.parvin@canonical.com>
2026-03-31 09:25:49 +0200

    test: add extra tests to errors package

--- internal/errors/errors_test.go
+++ internal/errors/errors_test.go
@@ -21,11 +21,13 @@ func TestCodefWrapsError(t *testing.T) {
 	err := errors.Codef(code, "%w", wrapped)
 	c.Check(err, qt.ErrorMatches, `an error happened`)
 	c.Check(errors.ErrorCode(err), qt.Equals, code)
+
 	errValue, ok := err.(*errors.Error)
 	c.Assert(ok, qt.IsTrue)
 	c.Check(errValue.Code, qt.Equals, code)
 	c.Assert(errValue.Err, qt.IsNotNil)
 	c.Check(errValue.Err.Error(), qt.Equals, wrapped.Error())
+	c.Check(errValue.Message, qt.Equals, "")
 	c.Check(stderr.Is(errValue.Err, wrapped), qt.IsTrue)
 	c.Check(stderr.Is(err, wrapped), qt.IsTrue)
 }
@@ -34,19 +36,34 @@ func TestCodefFormatsString(t *testing.T) {
 	c := qt.New(t)
 
 	code := errors.Code("test code")
-	err := errors.Codef(code, "an error happened")
-	c.Check(err, qt.ErrorMatches, `an error happened`)
-	c.Check(errors.ErrorCode(err), qt.Equals, code)
-	errValue, ok := err.(*errors.Error)
-	c.Assert(ok, qt.IsTrue)
-	c.Check(errValue.Code, qt.Equals, code)
-	c.Assert(errValue.Err, qt.IsNotNil)
-	c.Check(errValue.Err.Error(), qt.Equals, "an error happened")
-	c.Check(errValue.Message, qt.Equals, "")
-
-	err = errors.Codef(code, "formatted %s", "message")
+	err := errors.Codef(code, "formatted %s", "message")
 	c.Check(err, qt.ErrorMatches, `formatted message`)
 	c.Check(errors.ErrorCode(err), qt.Equals, code)
+}
+
+func TestErrorInfo(t *testing.T) {
+	c := qt.New(t)
+
+	info := map[string]any{"key": "value"}
+	err := error(&errors.Error{
+		Message: "error with info",
+		Info:    info,
+	})
+	c.Check(err, qt.ErrorMatches, `error with info`)
+	c.Check(errors.ErrorInfo(err), qt.DeepEquals, info)
+}
+
+func TestErrorMessageOrder(t *testing.T) {
+	c := qt.New(t)
+
+	err := &errors.Error{Code: errors.Code("a code")}
+	c.Check(err.Error(), qt.Equals, "a code")
+
+	err = &errors.Error{Err: errors.New("a wrapped err"), Code: errors.Code("a code")}
+	c.Check(err.Error(), qt.Equals, "a wrapped err")
+
+	err = &errors.Error{Message: "a message", Err: errors.New("a wrapped err"), Code: errors.Code("a code")}
+	c.Check(err.Error(), qt.Equals, "a message")
 }
 
 func TestErrorCodeWithJujuRPC(t *testing.T) {
